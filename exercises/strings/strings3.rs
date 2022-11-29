7bac519e0c9e458ac6af941a8c67d523cd68382d
mpab <mick_bythell@hotmail.com>
2022-11-29 22:59:12 +0100

    wip

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,21 +1,40 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.trim().to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+
+    // https://stackoverflow.com/questions/30154541/how-do-i-concatenate-strings
+    // https://maxuuell.com/blog/how-to-concatenate-strings-in-rust
+
+    //input.to_string().clone() + " world!"
+
+    // let mut ret = input.to_string();
+    // ret.push_str(" world!");
+    // ret
+
+    // let mut ret = input.to_owned();
+    // ret.push_str(" world!");
+    // ret
+
+    // let mut owned = input.to_owned();
+    // let ret = owned + " world!";
+    // ret
+
+    // format!("{} world!", input)
+
+    [input, " world!"].concat()
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
